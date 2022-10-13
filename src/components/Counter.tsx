17f82da27e6a100c525061b472e54977f82301e7
Wallace Júnior <wflj1997@hotmail.com>
2022-10-13 20:34:22 -0300

    add -10, -100, +10 and +100 buttons

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -5,24 +5,40 @@ interface CounterProps {}
 export const Counter: React.FC<CounterProps> = () => {
   const [count, setCount] = useState(0);
 
-  function increment() {
-    setCount(count => count + 1);
+  function increment(amount: number) {
+    setCount(count => count + amount);
   }
 
-  function decrement() {
-    setCount(count => count - 1);
+  function decrement(amount: number) {
+    setCount(count => count - amount);
   }
 
   return (
     <div className="wrapper">
-      <button title="Decrementar" onClick={decrement}>
-        -
+      <button title="Decrementar" onClick={() => decrement(100)}>
+        -100
+      </button>
+
+      <button title="Decrementar" onClick={() => decrement(10)}>
+        -10
+      </button>
+
+      <button title="Decrementar" onClick={() => decrement(1)}>
+        -1
       </button>
 
       <span>{count}</span>
 
-      <button title="Incrementar" onClick={increment}>
-        +
+      <button title="Incrementar" onClick={() => increment(1)}>
+        +1
+      </button>
+
+      <button title="Incrementar" onClick={() => increment(10)}>
+        +10
+      </button>
+
+      <button title="Incrementar" onClick={() => increment(100)}>
+        +100
       </button>
     </div>
   );
